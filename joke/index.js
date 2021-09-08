bd50f2c7bd5c716b834d754d93777b4d3f30dd97
marktariel <markevych09@gmail.com>
2021-09-08 20:39:16 +0200

    ajout du projet joke-app

---
+++ joke/index.js
@@ -0,0 +1,14 @@
+const header = document.getElementById('header');
+const content = document.getElementById('content');
+
+
+function getJoke () {
+    fetch("https://api.blablagues.net/?rub=blagues")
+      .then((res) => res.json())
+      .then((data) => {
+          header.textContent = data.data.content.text_head
+          content.textContent = data.data.content.text !== "" ? data.data.content.text : data.data.content.text_hidden;
+      })
+}
+
+document.body.addEventListener('click', getJoke);
